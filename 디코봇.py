b31e55220b5a27e5000c58fb6945f8efcd584215
choopsti <82592212+choopsti@users.noreply.github.com>
2021-09-20 02:30:17 +0900

    Add files via upload

---
+++ 디코봇.py
@@ -0,0 +1,119 @@
+import discord
+from discord.ext import commands
+from selenium.webdriver.chrome import options
+from youtube_dl import YoutubeDL
+import bs4
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from discord.utils import get
+from discord import FFmpegPCMAudio
+import asyncio
+import time
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print('다음으로 로그인합니다: ')
+    print(bot.user.name)
+    print('connection was succesful')
+    await bot.change_presence(status=discord.Status.online, activity=None)
+
+@bot.command()
+async def 명령어(ctx):
+    await ctx.send("명령어종류:명령어를 사용할때는 !치시고 노래는 !들어와를 사용후 해주세요(이미 들어와있으면 상관없음)\n!명령어 : 명령어를 알수있음\n!들어와 : 디스코드 봇이 음성채널에 들어간다\n!나가:디스코드 봇이 음성채널에 나간다\n!URL재생:URL이 재생된다\n!재생:노래가 재생된다\n!일시정지:정지된다!다시시작:다시시작된다\n!지금노래:지금노래 제목이 나온다\n!노래끄기:노래가 꺼진다\n!신승민_바보:신승민은 바보다")
+
+@bot.command()
+async def 들어와(ctx):
+    global vc
+    vc = await ctx.message.author.voice.channel.connect()
+
+@bot.command()
+async def 나가(ctx):
+    try:
+        await vc.disconnect()
+    except:
+        await ctx.send("....")
+
+@bot.command()
+async def URL재생(ctx, *, url):
+    YDL_OPTIONS = {'format': 'bestaudio','noplaylist':'True'}
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+    if not vc.is_playing():
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['formats'][0]['url']
+        vc.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        await ctx.send(embed = discord.Embed(title= "노래 재생", description = "현재 " + url + "을(를) 재생하고 있습니다.", color = 0x00ff00))
+    else:
+        await ctx.send("노래가 이미 재생되고 있습니다!")
+
+@bot.command()
+async def 재생(ctx, *, msg):
+    if not vc.is_playing():
+
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        global entireText
+        YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+            
+        chromedriver_dir = "C:/Users/nam11/OneDrive/바탕 화면/chromedriver_win32/chromedriver.exe"
+        driver = webdriver.Chrome(chromedriver_dir, options = options)
+        driver.get("https://www.youtube.com/results?search_query="+msg+"+lyrics")
+        source = driver.page_source
+        bs = bs4.BeautifulSoup(source, 'lxml')
+        entire = bs.find_all('a', {'id': 'video-title'})
+        entireNum = entire[0]
+        entireText = entireNum.text.strip()
+        musicurl = entireNum.get('href')
+        url = 'https://www.youtube.com'+musicurl 
+        driver.quit()
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['formats'][0]['url']
+        await ctx.send(embed = discord.Embed(title= "노래 재생", description = "현재 " + entireText + "을(를) 재생하고 있습니다.", color = 0x00ff00))
+        vc.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+    else:
+        await ctx.send("이미 노래가 재생 중이라 노래를 재생할 수 없어요!")
+
+
+@bot.command()
+async def 일시정지(ctx):
+    if vc.is_playing():
+        vc.pause()
+        await ctx.send(embed = discord.Embed(title= "일시정지", description = entireText + "을(를) 일시정지 했습니다.", color = 0x00ff00))
+    else:
+        await ctx.send("지금 노래가 재생되지 않네요.")
+
+@bot.command()
+async def 다시재생(ctx):
+    try:
+        vc.resume()
+    except:
+         await ctx.send("지금 노래가 재생되지 않네요.")
+    else:
+         await ctx.send(embed = discord.Embed(title= "다시재생", description = entireText  + "을(를) 다시 재생했습니다.", color = 0x00ff00))
+
+@bot.command()
+async def 노래끄기(ctx):
+    if vc.is_playing():
+        vc.stop()
+        await ctx.send(embed = discord.Embed(title= "노래끄기", description = entireText  + "을(를) 종료했습니다.", color = 0x00ff00))
+    else:
+        await ctx.send("지금 노래가 재생되지 않네요.")
+
+@bot.command()
+async def 지금노래(ctx):
+    if not vc.is_playing():
+        await ctx.send("지금은 노래가 재생되지 않네요..")
+    else:
+        await ctx.send(embed = discord.Embed(title = "지금노래", description = "현재 " + entireText + "을(를) 재생하고 있습니다.", color = 0x00ff00))
+
+@bot.command()
+async def 신승민_바보(ctx):
+    await ctx.send("신승민 바보")
+
+
+bot.run("ODg5MTA3MzAxOTY4MDE5NTA4.YUcbsw.w1iJxAr8Q2WiePE5iEmDeHTMpZs")
