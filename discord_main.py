bb642a5ad153ada41e4f0b03e292298f65df7d1a
choopsti <82592212+choopsti@users.noreply.github.com>
2021-09-23 21:12:31 +0900

    Update discord_main.py

--- discord_main.py
+++ discord_main.py
@@ -31,21 +31,20 @@ async def 재생(ctx, msg):
     messag1111 = msg
     global vc 
     vc = await ctx.message.author.voice.channel.connect()
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    a = 'https://www.google.com/search?q='+msg+'&oq='+msg+'&aqs=chrome..69i57j0i433i512j0i512l8.900j0j15&sourceid=chrome&ie=UTF-8'
-    data = requests.get(a,headers=headers)
-    bs4 = BeautifulSoup(data.text, 'html.parser')
-    test = bs4.find("h3", {"class": "H1u2de"})
-    test = test.select("a")
-    test = test[0]['href'] #메세지에 대한 영상링크 얻기
-    YDL_OPTIONS = {'format': 'bestaudio','noplaylist':'True'}
-    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-
     if not vc.is_playing():
+        headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+        a = 'https://www.google.com/search?q='+msg+'&oq='+msg+'&aqs=chrome..69i57j0i433i512j0i512l8.900j0j15&sourceid=chrome&ie=UTF-8'
+        data = requests.get(a,headers=headers)
+        bs4 = BeautifulSoup(data.text, 'html.parser')
+        test = bs4.find("h3", {"class": "H1u2de"})
+        test = test.select("a")
+        test = test[0]['href']
+        YDL_OPTIONS = {'format': 'bestaudio','noplaylist':'True'}
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         with YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(test, download=False)
         URL = info['formats'][0]['url']
-        vc.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS)) #youtube-dl로 받아서 mp3저장후 재생
+        vc.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
         await ctx.send(embed = discord.Embed(title= "노래 재생", description = "현재 " + msg + "을(를) 재생하고 있습니다.", color = 0x00ff00))
     else:
         await ctx.send("노래가 이미 재생되고 있습니다!")
